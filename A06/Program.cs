e9ddb4fa53691fd9c7da4531a07632539debe9f5
Gokul <gokul.thirumasanakumar@trumpf.com>
2025-11-18 16:25:51 +0530

    Review fix

--- A06/Program.cs
+++ A06/Program.cs
@@ -8,82 +8,93 @@
 using static System.Console;
 using System.Text;
 
+#region class Program  ----------------------------------------------------------------------------
 internal class Program {
-   private static void Main () {
-      Solve (0);
+   #region Methods --------------------------------------------------
+   static void Main () {
+      GenSolutions (0);
       PrintBoard ();
    }
 
-   static void Solve (int row) {
-      if (row == size) {
-         if (!IsDuplicate (board))
-            solutions.Add ([.. board]);
+   /// <summary>Generates multiple solutions with unique queen positions</summary>
+   static void GenSolutions (int row) {
+      if (row == Size) {
+         if (!IsDuplicate (sBoard)) sSolutions.Add ([.. sBoard]);
          return;
       }
-      for (int col = 0; col < size; col++) {
+      for (int col = 0; col < Size; col++) {
          if (IsSafe (row, col)) {
-            board[row] = col;
-            Solve (row + 1);
+            sBoard[row] = col;
+            GenSolutions (row + 1);
          }
       }
    }
 
+   /// <summary>Checks whether the queen is safe to place in
+   /// linear and diagonal directions</summary>
    static bool IsSafe (int row, int col) {
       for (int i = 0; i < row; i++) {
-         if (board[i] == col ||
-             board[i] - i == col - row ||
-             board[i] + i == col + row) {
+         if (sBoard[i] == col ||
+             sBoard[i] - i == col - row ||
+             sBoard[i] + i == col + row) {
             return false;
          }
       }
       return true;
    }
 
+   /// <summary>Returns whether a duplicate solution exists or not</summary>
    static bool IsDuplicate (int[] soln) {
       for (int i = 0; i < 4; i++) {
          soln = Rotate (soln);
+         // Checks if the solution already exists
          if (Exists (soln)) return true;
-         if (Exists ([.. soln.Select (x => size - 1 - x)])) return true;
+         // Checks for horizontal mirror duplicate solutions
+         if (Exists ([.. soln.Select (x => Size - 1 - x)])) return true;
+         // Checks for vertical mirror duplicate solutions
          if (Exists ([.. soln.Reverse ()])) return true;
       }
       return false;
 
-      bool Exists (int[] a) {
-         for (int i = 0; i < solutions.Count; i++)
-            if (solutions[i].SequenceEqual (a))
-               return true;
+      bool Exists (int[] arr) {
+         for (int i = 0; i < sSolutions.Count; i++)
+            if (sSolutions[i].SequenceEqual (arr)) return true;
          return false;
       }
 
-      int[] Rotate (int[] r) {
-         int[] temp = new int[size];
-         for (int i = 0; i < size; i++)
-            temp[r[i]] = size - 1 - i;
+      int[] Rotate (int[] arr) {
+         int[] temp = new int[Size];
+         for (int i = 0; i < Size; i++)
+            temp[arr[i]] = Size - 1 - i;
          return temp;
       }
    }
 
+   /// <summary>Prints the solution to the console</summary>
    static void PrintBoard () {
       OutputEncoding = Encoding.UTF8;
       int i = 0;
-      foreach (var soln in solutions) {
+      foreach (var soln in sSolutions) {
          Clear ();
          WriteLine ($"Solution {++i}\n┏━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┓");
-         for (int row = 0; row < size; row++) {
+         for (int row = 0; row < Size; row++) {
             Write ("┃");
-            for (int col = 0; col < size; col++)
+            for (int col = 0; col < Size; col++)
                Write ($" {(col == soln[row] ? "\u2655" : " ")} ┃");
-            if (row < size - 1)
-               Write ("\n┣━━━╋━━━╋━━━╋━━━╋━━━╋━━━╋━━━╋━━━┫\n");
+            if (row < Size - 1) Write ("\n┣━━━╋━━━╋━━━╋━━━╋━━━╋━━━╋━━━╋━━━┫\n");
          }
          Write ("\n┗━━━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━┛\n");
-         ReadKey ();
+         ReadKey (true);
       }
    }
+   #endregion
 
-   const int size = 8; // size of the board
+   #region Private variables ----------------------------------------
+   const int Size = 8; // size of the board
 
-   static int[] board = new int[size]; // board representation
+   static int[] sBoard = new int[Size]; // board representation
 
-   static List<int[]> solutions = []; // list to store unique solutions
+   static List<int[]> sSolutions = []; // list to store unique solutions
+   #endregion
 }
+#endregion
