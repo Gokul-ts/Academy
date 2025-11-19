21500498cc71f40b01e6dc541beafd067337b678
Gokul <gokul.thirumasanakumar@trumpf.com>
2025-11-19 11:41:35 +0530

    Minor fix

--- A04/Program.cs
+++ A04/Program.cs
@@ -7,30 +7,42 @@
 // ------------------------------------------------------------------------------------------------
 using static System.Console;
 
+#region class Program -----------------------------------------------------------------------------
 internal class Program {
+   #region Implementation -------------------------------------------
    static void Main () {
-      var chars = File.ReadAllText (@"TData\words.txt");
-      if (chars.Length == 0) {
-         WriteLine ("No words found in the file.");
-         return;
-      }
-      var fTable = BuildTable (chars);
-      WriteLine ("Letter | Occurrences\n--------------------");
-      int i = 0;
-      foreach (var item in fTable) {
-         if (i < 7) ForegroundColor = ConsoleColor.Green;
-         WriteLine ($"{item.Key,-6} | {item.Value}");
-         ResetColor ();
-         i++;
+      try {
+         var chars = File.ReadAllText (@"TData\words.txt");
+         if (chars.Length == 0) {
+            WriteLine ("No words found in the file.");
+            return;
+         }
+         WriteLine ("Letter | Occurrences\n--------------------");
+         int i = 0;
+         foreach (var (ch, occur) in BuildTable (chars)) {
+            if (i < 7) ForegroundColor = ConsoleColor.Green;
+            WriteLine ($"{ch,-6} | {occur}");
+            ResetColor ();
+            i++;
+         }
+      } catch {
+         WriteLine ("Error reading file!");
       }
    }
+   #endregion
 
+   #region Methods --------------------------------------------------
+   // Builds a frequency table for alphabetic characters
    static Dictionary<char, int> BuildTable (string chars) {
       foreach (var ch in chars.ToUpper ())
          if (ch is >= 'A' and <= 'Z')
-            if (!freqTable.TryAdd (ch, 1)) freqTable[ch]++;
-      return freqTable.OrderByDescending (a => a.Value).ToDictionary ();
+            if (!sFreqTable.TryAdd (ch, 1)) sFreqTable[ch]++;
+      return sFreqTable.OrderByDescending (a => a.Value).ToDictionary ();
    }
+   #endregion
 
-   static Dictionary<char, int> freqTable = [];
+   #region Private data ---------------------------------------------
+   static Dictionary<char, int> sFreqTable = [];
+   #endregion
 }
+#endregion
