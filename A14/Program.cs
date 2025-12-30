161f62992a5f33b329b9fcf6622fb89618638143
Gokul <gokul.thirumasanakumar@trumpf.com>
2025-12-30 11:38:12 +0530

    A14: Initial implementation

---
+++ A14/Program.cs
@@ -0,0 +1,26 @@
+﻿// ------------------------------------------------------------------------------------------------
+// Training ~ A training program for new joinees at Metamation, Batch - July 2025.
+// Copyright (c) Metamation India.
+// ------------------------------------------------------------------
+// Program.cs
+// Program to find all anagrams from an input text file and sort them based on count.
+// ------------------------------------------------------------------------------------------------
+using static System.Console;
+
+internal class Program {
+   static void Main () {
+      try {
+         Dictionary<string, List<string>> data = [];
+         foreach (var word in File.ReadAllLines (@"data\words.txt")) {
+            string key = new ([.. word.Order ()]);
+            if (data.TryGetValue (key, out var wList))
+               wList.Add (word);
+            else data.Add (key, []);
+         }
+         data = data.Where (a => a.Value.Count > 1).OrderByDescending (a => a.Value.Count).ToDictionary ();
+         foreach (var val in data.Values) WriteLine ($"{val.Count} {string.Join (' ', val)}");
+      } catch (Exception) {
+         WriteLine ("Couldn't read file!!");
+      }
+   }
+}
